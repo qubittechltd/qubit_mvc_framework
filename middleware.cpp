491a04333c078a6d8bcbf6724c92d48b5aaee113
Ibrahim <ceo@qubittech.co.tz>
2024-10-16 17:35:22 +0300

    no message

---
+++ middleware.cpp
@@ -0,0 +1,118 @@
+#include "qubit_mvc_framework/utilities/middleware.h"
+#include "utilities/cachefiles.h"
+#include "qubit_mvc_framework/utilities/common_p.h"
+#include "qubit_mvc_framework/utilities/redirect.h"
+#include <QtHttpServer/QHttpServer>
+#include <QtHttpServer/QHttpServerRouterRule>
+#include <qubit_mvc_framework/utilities/session.h>
+
+void urlHandler(QObject *server,const QString &path,QHttpServerResponder &responder){
+    try{
+        try {
+            auto & c = CacheFiles::load_file(server,path);
+            QHttpServerResponse response(c.params().data);
+            if(path.endsWith(".css")){
+                response.addHeader("Content-Type","text/css");
+            }else if(path.endsWith(".js")){
+                response.addHeader("Content-Type","text/javascript");
+            }
+            responder.sendResponse(response);
+        }catch (MVC_FILE_STREAM_REQUIRED & e) {
+            responder.sendResponse(QHttpServerResponse::fromFile(e.path));
+        }catch (MVC_FILE_NOT_FOUND & e) {
+            urlHandler(server,"/errors/error_404.html",responder);
+        }
+        catch (MVC_FILE_PERMISSION_FAILED & e) {
+            urlHandler(server,"/errors/error_403.html",responder);
+        }
+        catch (MVC_FILE_READ_ERROR & e) {
+            urlHandler(server,"/errors/error_500.html",responder);
+        }
+        catch (MVC_SESSION_EXPIRED & e) {
+            responder.sendResponse(QHttpServerResponse(redirect("/login"),QHttpServerResponder::StatusCode::MovedPermanently));
+        }
+        catch (MVC_USER_UNAUTHORIZED & e) {
+            responder.sendResponse(QHttpServerResponse(redirect("/login"),QHttpServerResponder::StatusCode::MovedPermanently));
+        }
+    }catch(std::exception &e){
+        qDebug("system_error %s",e.what());
+    }
+}
+
+void MIDDLEWARE::post_middleware(const QHttpServerRequest &request, QHttpServerResponder &&responder,RouterHandler routerHandler){
+    auto server =  (QHttpServer *) parent();
+    try{
+        bool session_found;
+        auto session = Session::user_session_t(request);
+
+        auto response = routerHandler(request,*std::get<0>(session));
+        ///after controller // mid _ middleware
+        if(!std::get<1>(session)){
+            response.addHeaders({
+                {"Set-Cookie",std::get<0>(session)->getCookie()},
+            });
+        }
+        responder.sendResponse(response);
+        //after response  // post middleware
+        //histroy taking
+
+    }catch (ControllerDeletedException & e) {
+        urlHandler(server,"/errors/error_500.html",responder);
+    }catch(ControllerTimeOutException & e){
+        urlHandler(server,"/errors/error_408.html",responder);
+    }catch(std::exception & e){
+        qDebug("return stacktrace web page std::exception %s",e.what());
+    }
+}
+
+MIDDLEWARE::MIDDLEWARE(QHttpServer *server) : QObject(server){
+
+    server->setMissingHandler([server](const QHttpServerRequest &request,QHttpServerResponder &&responder){
+        auto path = request.url().path();
+        urlHandler(server,path,responder);
+    });
+
+    //WEB
+    server->beforeController([server, this](bool &handled,const QHttpServerRequest &request,QHttpServerResponder &responder){
+        if(!doMiddleWare<MIDDLEWARE::WEB>(request)){
+            return true;
+        }
+        auto session = request.session();
+        session->history(request.url().path().toStdString());
+        Q_UNUSED(session)
+        return true;
+    });
+
+    //AUTH
+    server->beforeController([server, this](bool &handled,const QHttpServerRequest &request,QHttpServerResponder &responder){
+        if(!doMiddleWare<MIDDLEWARE::AUTH>(request)){
+            return true;
+        }
+        if(!request.session()->user()){
+            responder.writeStatusLine(QHttpServerResponder::StatusCode::Found);
+            responder.writeHeaders({
+                {"Location", "/login"}
+            });
+            responder.writeBody("\r\n\r\n");
+            handled=true;
+            return false;
+        }
+        return true;
+    });
+}
+
+QHttpServer & MIDDLEWARE::server() const{
+    return *static_cast<QHttpServer*>(parent());
+}
+
+MIDDLEWARE::COMMON operator|(const MIDDLEWARE::COMMON c, const MIDDLEWARE::COMMON d){
+    auto a = (qintptr)c;
+    auto b = (qintptr)c;
+    return MIDDLEWARE::COMMON(a | b);
+}
+
+MIDDLEWARE::COMMON operator&(const MIDDLEWARE::COMMON c, const MIDDLEWARE::COMMON d){
+    auto a = (qintptr)c;
+    auto b = (qintptr)d;
+    return MIDDLEWARE::COMMON(a & b);
+}
---
+++ session.cpp
@@ -0,0 +1,136 @@
+#include "include/qubit_mvc_framework/utilities/session.h"
+#include "qubit_mvc_framework/utilities/common_p.h"
+#include "QCoreApplication"
+#include "utilities/secure.h"
+#include <QThread>
+#include <QUuid>
+#include <vector>
+
+std::vector<Session*> sessions;//multithreading
+bool operator == (const Session *c ,const QByteArray &token){
+    return c->token == token;
+}
+Session::Session(const QByteArray &t,const QDeadlineTimer & rememberMe, QObject *parent)
+    : token(t),remember_me(rememberMe),QObject{parent}, _user(UserModel::Default())
+{
+     ::sessions.push_back(this);
+}
+
+Session::~Session()
+{
+    auto itr = std::find(::sessions.begin(),::sessions.end(),this);
+    if(itr != ::sessions.end()){
+        ::sessions.erase(itr);
+    }
+}
+
+const Session * QHttpServerRequest::session() const{
+   return Session::user_session(*this,QDeadlineTimer(std::chrono::days(REMEMEMBER_ME_DAYS)));
+}
+
+QByteArray generateTokenFromPPK(){
+    auto uuidB64 = QUuid::createUuid().toByteArray().toBase64(QByteArray::OmitTrailingEquals|QByteArray::Base64UrlEncoding);
+    QByteArray token;
+    if(!Secure::instance().signBytes(std::ref(token),uuidB64)){
+       return "";
+    }
+    return token;
+}
+bool isSessionFromThisInstance(const QByteArray &token){
+    //if the app crashes or unexceptly shutdown, all session are invalidated
+    if(!Secure::instance().verifySignature(token)){
+        return true;
+    }
+    return false;
+}
+bool hasAuthTokenBeenTemped(const QByteArray &token){
+    if(!Secure::instance().verifySignature(token)){
+        return true;
+    }
+    return false;
+}
+
+bool token_has_exipired(const QByteArray &token){
+    return false;
+}
+
+void Session::set_user(UserModel & u){
+    _user = u;
+}
+
+std::tuple<Session *, bool> Session::user_session_t(const QHttpServerRequest &request, const QDeadlineTimer &rememberMe)
+{
+    if(QThread::currentThread() != QCoreApplication::instance()->thread()){
+        qWarning()<<"Session::user_session is called in non mainthread";
+    }
+
+    QByteArray user_token = std::visit([&] -> QByteArray{//
+        for (const auto & p : request.headers()) {
+            if(p.first == "Cookie"){
+                for (auto &name_value : p.second.split(';')) {
+                    name_value = name_value.trimmed();
+                    const QByteArray session("session=");
+                    if(name_value.startsWith(session)){
+                        return name_value.last(name_value.size() - session.size());
+                    }
+                }
+            }
+        }
+        return "";
+    });
+
+    bool skip_search = std::visit([&user_token] -> bool{
+        if(user_token.isEmpty())
+            return true;
+        if(token_has_exipired(user_token))
+            return true;
+        if(isSessionFromThisInstance(user_token))
+            return true;
+        return false;
+    });
+
+    auto itr = std::find_if((skip_search ? sessions.end(): sessions.begin()),sessions.end(),[&user_token](Session * session){
+        if(session->token == user_token){
+            session->hits++;
+            return true;
+        }else if(session->remember_me.hasExpired()){
+            session->deleteLater();
+        }
+        return false;
+    });
+
+    bool session_found(itr == sessions.end());
+    Session * session = session_found ?
+                            new Session(generateTokenFromPPK(),rememberMe) :
+                                *itr;
+
+    return std::make_tuple(session,session_found);
+}
+
+UserModel Session::user() const {
+    return _user;
+}
+void Session::logout(){
+    _user.reset();
+
+}
+
+QByteArray Session::getCookie(){
+    auto expirationDateTime = QDateTime::currentDateTime()+std::chrono::milliseconds(remember_me.remainingTime());
+    QString expires = "expires=" + expirationDateTime.toString(Qt::RFC2822Date);
+
+    QString path = "path=/";
+    QString domain = "" PROJECT_DOMAIN "";
+    QString httpOnly = "HttpOnly";
+    // QString secure = "secure";
+    // QString SameSite="SameSite=None";
+    return QString("session=%1; %2; %3; %4; %5;")
+                .arg(token,expires,path,domain,httpOnly).toLocal8Bit();
+}
+
+std::string Session::prev_page(){
+    return history().size() ? "/" : history().back();
+}
+
+#include <session.moc>
+
