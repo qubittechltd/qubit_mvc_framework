bc62a6cb1034593253f93227c01b8719ac62106a
Sunday Phery <125968708+sundayphery@users.noreply.github.com>
2024-10-16 16:00:39 +0300

    finalizing models

--- include/qubit_mvc_framework/utilities/model/model.h
+++ include/qubit_mvc_framework/utilities/model/model.h
@@ -1,8 +1,8 @@
 #pragma once
 #ifndef MODEL_H
 #define MODEL_H
-#include "utilities/model/table_base_impl.h"
-#include "utilities/model/table_base.h"
+#include "qubit_mvc_framework/utilities/model/table_base_impl.h"
+#include "qubit_mvc_framework/utilities/model/table_base.h"
 #include "qubit_mvc_framework/utilities/model/model_base.h"
 #include "qubit_mvc_framework/utilities/model/sql_query.h"
 #endif // MODEL_H
