8598a585c434c5c0410a907ed03a78b1d1348d5c
Ibrahim <ceo@qubittech.co.tz>
2024-10-18 16:49:13 +0300

    building working

--- include/qubit_mvc_framework/config/app.h
+++ include/qubit_mvc_framework/config/app.h
@@ -5,9 +5,6 @@
 #include <QTimeZone>
 
 struct QUBIT_MVC_APP {
-    // static const QUBIT_MVC_APP * instance(){
-    //     return nullptr;
-    // }
     static const QUBIT_MVC_APP * instance();
     QTimeZone timezone() const;
     QString database_datetime_format() const;
