d3de7cc1ad4f1f7d447e821398a49907f8b81013
Ibrahim <ceo@qubittech.co.tz>
2024-10-18 16:40:31 +0300

    no message

--- app.cpp
+++ app.cpp
@@ -1,25 +1,25 @@
 #include "qubit_mvc_framework/config/app.h"
 
 
-inline QTimeZone QUBIT_MVC_APP::timezone() const{
+QTimeZone QUBIT_MVC_APP::timezone() const{
     return QTimeZone::utc();
 }
-inline QString QUBIT_MVC_APP::database_datetime_format() const{
+QString QUBIT_MVC_APP::database_datetime_format() const{
     return "yyyy-MM-dd hh:mm:ss";
 }
 
-inline int QUBIT_MVC_APP::job_queue_limit() const{
+int QUBIT_MVC_APP::job_queue_limit() const{
     return 5;
 }
 
-inline int QUBIT_MVC_APP::tries() const{
+int QUBIT_MVC_APP::tries() const{
     return 5;
 }
 
-inline int QUBIT_MVC_APP::retry_after() const{
+int QUBIT_MVC_APP::retry_after() const{
     return 5;
 }
 
-inline int QUBIT_MVC_APP::model_cache_time() const{
+int QUBIT_MVC_APP::model_cache_time() const{
     return 5000;
 }
